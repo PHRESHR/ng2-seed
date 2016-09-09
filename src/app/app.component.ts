86539a7a0a6aac4671208ebb2a5562f5c47b87ae
DeAndre Johnson <dre@phreshr.com>
2016-09-09 16:36:33 -0400

    updated to use styles and added counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,40 @@
 import '@ngrx/core/add/operator/select';
 import 'rxjs/add/operator/map';
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import 'rxjs/add/operator/take';
+import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { INCREMENT, DECREMENT, RESET, AppState } from './shared/counter';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'PHRESHR';
+  counter: Observable<number>;
 
-  constructor(private titleService: Title) {
+  constructor(
+    public store: Store<AppState>,
+    private titleService: Title) {
     this.titleService.setTitle(this.title);
+    this.counter = store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
   }
 }
