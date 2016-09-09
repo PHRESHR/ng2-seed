1c7bcfec1769b37684610c00ade1f6648d3e2b0a
DeAndre Johnson <dre@phreshr.com>
2016-09-09 16:29:55 -0400

    Added HMR

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,9 +1,25 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { decorateModuleRef } from './app/environment';
 
+// HMR
+import { handleHmr } from './hmr';
+
+// Root module
 import { AppModule } from './app';
 
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .then(decorateModuleRef)
-    .catch(err => console.error(err));
+function bootstrap() {
+  return platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .then(decorateModuleRef)
+      .catch(err => console.error(err));
+}
+
+// We may bootstrap the app in one of two ways..
+if ((<any>module).hot) {
+  // A hot loading bootstrap. Delegate to handleHmr, see hmr.ts.
+  handleHmr(module, bootstrap);
+} else {
+  // A regular (cold) bootstrap, when not doing hot loading. We just call
+  // Angular's bootstrap directly from this module.
+  bootstrap();
+}
