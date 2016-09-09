ee87a5666bec3cbb541d4ff3f7a26d472a7f08f3
DeAndre Johnson <dre@phreshr.com>
2016-09-09 16:34:19 -0400

    Removed unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
