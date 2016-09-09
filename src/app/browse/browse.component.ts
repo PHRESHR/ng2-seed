ded4b741c0072f90f84f4ac256ec07666d2aaab5
DeAndre Johnson <dre@phreshr.com>
2016-09-09 17:59:40 -0400

    Added mock data from Apollo server

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Observable } from 'rxjs/Observable';
+import { Apollo, Angular2Apollo, ApolloQueryObservable } from 'angular2-apollo';
+// We need this to parse graphql string
+import gql from 'graphql-tag';
+
+import { client } from '../client.ts';
+
+const query = gql`
+  query getString {
+    testString
+  }
+`;
 
 @Component({
   selector: 'phrsr-browse',
   templateUrl: 'browse.component.html',
   styleUrls: ['browse.component.css']
+})
+@Apollo({
+  client,
+  queries() {
+    return {
+      data: { query },
+    };
+  }
 })
 export class BrowseComponent implements OnInit {
   title = 'Browse';
