1e2cfaa71080d1914e8107fc87af9db1f0339dc7
DeAndre Johnson <dre@phreshr.com>
2016-09-16 11:39:33 -0400

    Removed Apollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ApolloModule } from 'angular2-apollo';
 
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
@@ -9,7 +8,6 @@ import { SharedModule } from './shared/shared.module';
 import { BrowseModule } from './browse/browse.module';
 import { AboutModule } from './about/about.module';
 import { NotFoundModule } from './not-found/not-found.module';
-import { client } from './client';
 
 @NgModule({
   declarations: [
@@ -17,7 +15,6 @@ import { client } from './client';
   ],
   imports: [
     BrowserModule,
-    ApolloModule.withClient(client),
     AppRoutes,
     BrowseModule,
     AboutModule,
